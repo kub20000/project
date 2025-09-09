2beabb35ccae14da9399b50b74e70487ea1ea123
k9k8h <rkdrjsgh123@naver.com>
2025-09-09 14:26:36 +0900

    Add files via upload

---
+++ TestPJ/src/main/java/com/community/post/controller/PostController.java
@@ -0,0 +1,92 @@
+package com.community.post.controller;
+
+import com.community.post.entity.Post;
+import com.community.post.service.PostService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@Controller
+public class PostController {
+
+    private final PostService postService;
+
+    public PostController(PostService postService) {
+        this.postService = postService;
+    }
+
+    // 메인에 게시글 리스트 출력
+    @GetMapping("/post/list")
+    public String postList(Model model) {
+        System.out.println("postList");
+        model.addAttribute("posts", postService.findAll());
+        return "main";
+    }
+
+    // 게시글 추가(페이지 연결)
+    @GetMapping("/post/add")
+    public String addForm(Model model) {
+        model.addAttribute("post", new Post());
+        return "post/addPost";
+    }
+
+    // 게시글 추가(버튼 클릭하여 실제로 게시글 추가)
+    @PostMapping("/post/add")
+    public String addPost(@ModelAttribute Post post) {
+        postService.add(post);
+        return "redirect:/post/list";
+    }
+
+    // 게시글 보기 (제목(title) 클릭해서 연결되는 페이지) -> 9.9 <br> 태그 문제 해결
+    @GetMapping("/post/detail/{id}")
+    public String postDetailForm(@PathVariable int id, Model model){
+        Post post = (Post) postService.findById(id).orElseThrow(
+                ()->new IllegalArgumentException("Invaild id"));
+        String content = Optional.ofNullable(post.getContent()).orElse("");
+        String contentWithBr = content.replace("\n", "<br>");
+        model.addAttribute("post",post);
+        model.addAttribute("contentWithBr",contentWithBr);
+        System.out.println("post : "+post);
+
+        // 2. 다음/이전 게시글 ID 찾기
+        Optional<Integer> nextId = postService.findNextPostId(id);
+        Optional<Integer> prevId = postService.findPrevPostId(id);
+
+        System.out.println("Next ID: " + nextId);
+        System.out.println("Prev ID: " + prevId);
+
+        // 3. Optional 객체에서 값 가져와서 모델에 추가
+        nextId.ifPresent(next -> model.addAttribute("nextId", next));
+        prevId.ifPresent(prev -> model.addAttribute("prevId", prev));
+
+        return "post/detailPost";
+    }
+
+    // 게시글 삭제
+    @PostMapping("/post/delete")
+    public String deletePost(@RequestParam int id) {
+        postService.deleteById(id);
+        return "redirect:/post/list";
+    }
+
+    //게시글 수정(수정 페이지(editPost)로 연결)
+    @GetMapping("/post/edit/{id}")
+    public String postEditForm(@PathVariable int id, Model model){
+        Post post = (Post) postService.findById(id).orElseThrow(
+                ()->new IllegalArgumentException("Invaild post id "));
+        model.addAttribute("post",post);
+        System.out.println("post : "+post);
+        return "post/editPost";
+    }
+
+    //게시글 수정(버튼 클릭하여 실제로 수정)
+    @PostMapping("/post/edit")
+    public String userEdit(@ModelAttribute Post post){
+        postService.update(post);
+        return "redirect:/post/list";
+    }
+
+
+}
---
+++ TestPJ/src/main/java/com/community/post/repository/PostRepo.java
@@ -0,0 +1,107 @@
+package com.community.post.repository;
+
+import com.community.post.entity.Post;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public class PostRepo {
+    private final JdbcTemplate jdbc;
+
+    @Autowired
+    public PostRepo(JdbcTemplate jdbc) {
+        this.jdbc = jdbc;
+    }
+
+    //게시글 등록
+    public Post add(Post post) {
+        System.out.println("==> post added");
+        String sql = "INSERT INTO posts (id, title, author, content, created_at) " +
+                " VALUES (?,?,?,?,?)";;
+        int result = jdbc.update(sql,
+                post.getId(),
+                post.getTitle(),
+                post.getAuthor(),
+                post.getContent(),
+                post.getCreated_at()
+        );
+        if (result == 1) {
+            System.out.println(result);
+            System.out.println("게시글 등록 성공");
+            return post;
+        } else {
+            System.out.println("게시글 등록 실패");
+            return null;
+        }
+    }
+
+    // 모든 게시글 출력
+    public List<Post> findAll() {
+        String sql = "SELECT * FROM posts";
+        return jdbc.query(sql,postRowMapper());
+        }
+
+    // 게시글 매핑(출력 전 매핑)
+    private RowMapper<Post> postRowMapper() {
+        return (rs, rowNum) -> {
+            java.sql.Timestamp timestamp = rs.getTimestamp("created_at");
+            LocalDateTime createdAt = (timestamp != null) ? timestamp.toLocalDateTime() : null;
+            return new Post(
+                    rs.getInt("id"),
+                    rs.getString("author"),
+                    rs.getString("title"),
+                    rs.getString("content"),
+                    createdAt
+            );
+        };
+    }
+
+    // 특정 아이디 값의 데이터 가져오기
+    public Optional<Post> findById(int id) {
+        System.out.println("==> post findById" + id);
+        String sql = "SELECT * FROM posts WHERE id = ?";
+        Post post = jdbc.queryForObject(sql, postRowMapper(), id);
+        System.out.println("==> post : " + id);
+        return Optional.ofNullable(post);
+    }
+
+    // 게시글 삭제 (게시글이 없으면 삭제 불가)
+    public void deleteById(int id) {
+        String sql = "DELETE FROM posts WHERE id = ?";
+        int rowAffected = jdbc.update(sql, id);
+        if (rowAffected > 0) {
+            System.out.println(rowAffected);
+        } else {
+            System.out.println("삭제할 게시글이 없습니다." + id);
+        }
+    }
+
+    // 게시글 수정
+    public void update(Post post) {
+        String sql = "UPDATE posts SET title = ?, content = ? WHERE id = ?";
+        jdbc.update(sql,
+                post.getTitle(),
+                post.getContent(),
+                post.getId()
+        );
+    }
+
+    public Optional<Integer> findNextId(int postId) {
+        String sql = "SELECT MIN(id) FROM posts WHERE id > ?";
+        Integer nextId = jdbc.queryForObject(sql, Integer.class, postId);
+        return Optional.ofNullable(nextId);
+    }
+
+    public Optional<Integer> findPrevId(int postId) {
+        String sql = "SELECT MAX(id) FROM posts WHERE id < ?";
+        Integer prevId = jdbc.queryForObject(sql, Integer.class, postId);
+        return Optional.ofNullable(prevId);
+    }
+}
+
