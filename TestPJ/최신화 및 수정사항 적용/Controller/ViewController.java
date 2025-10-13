c1210361ce61d799ad933beb0c1379e5b25d14ac
k9k8h <rkdrjsgh123@naver.com>
2025-10-13 15:26:51 +0900

    Add files via upload

--- TestPJ/최신화 및 수정사항 적용/Controller/ViewController.java
+++ TestPJ/최신화 및 수정사항 적용/Controller/ViewController.java
@@ -3,6 +3,7 @@ package com.bproject.web;
 
 import com.bproject.course.entity.Course;
 import com.bproject.course.service.CourseService;
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -48,7 +49,15 @@ public class ViewController {
 
 
     @GetMapping("courses")
-    public String course() {return "/courses/courses";}
+    public String course(HttpSession session) {
+
+        if (session.getAttribute("loginUser") == null) {
+            // 미로그인 시 로그인 페이지로 리다이렉트
+            return "redirect:/login";
+        }
+
+        return "/courses/courses";
+    }
 
 //    @GetMapping("post/main")
 //    public String postMain(){
